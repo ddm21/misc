be35d505450b0f76f0a3e4cceae82bc6a12a2113
dhruvdalsaniya21 <98445934+dhruvdalsaniya21@users.noreply.github.com>
2023-06-26 11:49:55 +0530

    raw_install_erpnext14.txt

--- install-erpnext14.sh
+++ install-erpnext14.sh
@@ -1,182 +1,124 @@
-#!/bin/bash
+Install Frappe/ERPNext version 14 in Ubuntu 22.04 LTS
 
-# Variables
-GIT_INSTALLATION="sudo apt-get install -y git"
-PYTHON_DEV_INSTALLATION="sudo apt-get install -y python3-dev"
-SETUPTOOLS_PIP_INSTALLATION="sudo apt-get install -y python3-setuptools python3-pip"
-VIRTUALENV_INSTALLATION="sudo apt-get install -y virtualenv && sudo apt-get install -y python3.10-venv"
-MARIADB_INSTALLATION="sudo apt-get install -y software-properties-common && sudo apt-get install -y mariadb-server"
-SECURE_MARIADB="sudo mysql_secure_installation"
-MYSQL_DEV_FILES_INSTALLATION="sudo apt-get install -y libmysqlclient-dev"
-REDIS_INSTALLATION="sudo apt-get install -y redis-server"
-NODEJS_INSTALLATION="sudo apt-get install -y curl && curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash && source ~/.profile && nvm install 16"
-YARN_INSTALLATION="sudo apt-get install -y npm && sudo npm install -g yarn"
-WKHTMLTOPDF_INSTALLATION="sudo apt-get install -y xvfb libfontconfig wkhtmltopdf"
-FRAPPE_BENCH_INSTALLATION="sudo -H pip3 install frappe-bench==5.10.1"
-FRAPPE_BRANCH="version-14"
-SITE_NAME="dcode.com"
-ERPNEXT_APP_INSTALLATION="bench get-app payments && bench get-app erpnext --branch version-14 && bench --site dcode.com install-app erpnext"
-PRODUCTION_SETUP="sudo bench setup production dcode-frappe"
-CREATE_USER="sudo adduser dcode-frappe && sudo usermod -aG sudo dcode-frappe"
-SSL_CERTBOT_INSTALLATION="sudo apt-get install -y certbot python3-certbot-nginx"
-SSL_DOMAIN_NAME="{domain_name}"
-SSL_CERTBOT_RENEWAL="sudo certbot renew --dry-run"
+# STEP 1 Install git
+sudo apt-get install git
 
-# Function to display success message with emoji
-function show_success_message {
-    echo -e "\e[32m✅ $1\e[0m"
-}
+# STEP 2 install python-dev
+sudo apt-get install python3-dev
 
-# Function to display error message with emoji
-function show_error_message {
-    echo -e "\e[31m❌ $1\e[0m"
-}
+# STEP 3 Install setuptools and pip (Python's Package Manager).
+sudo apt-get install python3-setuptools python3-pip
 
-# Function to display information message with emoji
-function show_info_message {
-    echo -e "\e[33m🔔 $1\e[0m"
-}
+# STEP 4 Install virtualenv
+sudo apt-get install virtualenv
+sudo apt install python3.10-venv
 
-# Function to display prompt message
-function show_prompt_message {
-    echo -e "\e[34m❓ $1\e[0m"
-}
+# STEP 5 Install MariaDB
+sudo apt-get install software-properties-common
+sudo apt install mariadb-server
+after running this command it will as few question to setup mariadb which i need to be asked in terminal and get the answer
+	Enter current password for root (enter for none): # PRESS ENTER
+	Switch to unix_socket authentication [Y/n] Y
+	Change the root password? [Y/n] Y
+	Remove anonymous users? [Y/n] Y
+	Disallow root login remotely? [Y/n] Y
+	Remove test database and access to it? [Y/n] Y
+	Reload privilege tables now? [Y/n] Y
 
-# STEP 1: Install git
-show_info_message "Installing git..."
-$GIT_INSTALLATION
-show_success_message "Git installed successfully!"
+# STEP 6 MySQL database development files
+sudo apt-get install libmysqlclient-dev
 
-# STEP 2: Install python-dev
-show_info_message "Installing python-dev..."
-$PYTHON_DEV_INSTALLATION
-show_success_message "Python-dev installed successfully!"
-
-# STEP 3: Install setuptools and pip
-show_info_message "Installing setuptools and pip..."
-$SETUPTOOLS_PIP_INSTALLATION
-show_success_message "Setuptools and pip installed successfully!"
-
-# STEP 4: Install virtualenv
-show_info_message "Installing virtualenv..."
-$VIRTUALENV_INSTALLATION
-show_success_message "Virtualenv installed successfully!"
-
-# STEP 5: Install MariaDB
-show_info_message "Installing MariaDB..."
-$MARIADB_INSTALLATION
-show_success_message "MariaDB installed successfully!"
-
-# Secure MariaDB installation
-show_prompt_message "Do you want to secure the MariaDB installation? (Y/n)"
-read -r secure_mariadb
-
-if [[ $secure_mariadb =~ ^[Yy]$ ]]; then
-    $SECURE_MARIADB
-fi
-
-# STEP 6: Install MySQL database development files
-show_info_message "Installing MySQL database development files..."
-$MYSQL_DEV_FILES_INSTALLATION
-show_success_message "MySQL database development files installed successfully!"
-
-# STEP 7: Edit the mariadb configuration
-show_info_message "Editing mariadb configuration..."
+# STEP 7 Edit the mariadb configuration ( unicode character encoding )
 sudo nano /etc/mysql/mariadb.conf.d/50-server.cnf
-# Add the required configuration to 50-server.cnf file
 
-show_prompt_message "Press Enter after making the necessary changes in the mariadb configuration file..."
-read -r enter_key
+add this to the 50-server.cnf file
 
+[server]
+user = mysql
+pid-file = /run/mysqld/mysqld.pid
+socket = /run/mysqld/mysqld.sock
+basedir = /usr
+datadir = /var/lib/mysql
+tmpdir = /tmp
+lc-messages-dir = /usr/share/mysql
+bind-address = 127.0.0.1
+query_cache_size = 16M
+log_error = /var/log/mysql/error.log
+
+[mysqld]
+innodb-file-format=barracuda
+innodb-file-per-table=1
+innodb-large-prefix=1
+character-set-client-handshake = FALSE
+character-set-server = utf8mb4
+collation-server = utf8mb4_unicode_ci      
+ 
+[mysql]
+default-character-set = utf8mb4
+
+# comment anything if any duplicate configuration found by default and paste this config insted
+
+# Now press (Ctrl-X) to exit
 sudo service mysql restart
-show_success_message "Mariadb configuration updated successfully!"
 
-# STEP 8: Install Redis
-show_info_message "Installing Redis..."
-$REDIS_INSTALLATION
-show_success_message "Redis installed successfully!"
+# STEP 8 install Redis
+sudo apt-get install redis-server
 
-# STEP 9: Install Node.js 14.X package
-show_info_message "Installing Node.js 14.X..."
-$NODEJS_INSTALLATION
-show_success_message "Node.js 14.X installed successfully!"
+# STEP 9 install Node.js 14.X package
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash &&
+apt-get install nodejs -y
 
-# STEP 10: Install Yarn
-show_info_message "Installing Yarn..."
-$YARN_INSTALLATION
-show_success_message "Yarn installed successfully!"
+# STEP 10 install Yarn
+sudo apt-get install npm
+sudo npm install -g yarn
 
-# STEP 11: Install wkhtmltopdf
-show_info_message "Installing wkhtmltopdf..."
-$WKHTMLTOPDF_INSTALLATION
-show_success_message "wkhtmltopdf installed successfully!"
+# STEP 11 install wkhtmltopdf
+sudo apt-get install xvfb libfontconfig wkhtmltopdf
 
-# STEP 12: Install frappe-bench
-show_info_message "Installing frappe-bench..."
-$FRAPPE_BENCH_INSTALLATION
-show_success_message "frappe-bench installed successfully!"
+# STEP 12 install frappe-bench
+sudo -H pip3 install frappe-bench
+bench --version
 
-# Verify frappe-bench installation
-bench_version=$(bench --version)
-
-if [[ $? -eq 0 ]]; then
-    show_success_message "frappe-bench version: $bench_version"
-else
-    show_error_message "frappe-bench installation failed!"
-    exit 1
-fi
-
-# STEP 13: Initialize the frappe bench & install frappe latest version
-show_info_message "Initializing frappe-bench..."
-bench init frappe-bench --frappe-branch "$FRAPPE_BRANCH"
+# STEP 13 initilise the frappe bench & install frappe latest version
+bench init frappe-bench --frappe-branch version-14
 cd frappe-bench/
-show_success_message "frappe-bench initialized successfully!"
+bench start
 
-# Start frappe-bench
-show_prompt_message "Do you want to start the frappe-bench? (Y/n)"
-read -r start_bench
+# STEP 14 create a site in frappe bench
+# ask user what site-name they want to give and update the command with the {site-name} and run the bench command
+bench new-site {site-name}
+bench use {site-name}
 
-if [[ $start_bench =~ ^[Yy]$ ]]; then
-    bench start
-fi
 
-# STEP 14: Create a site in frappe bench
-show_info_message "Creating a new site in frappe-bench..."
-bench new-site "$SITE_NAME"
-bench use "$SITE_NAME"
-show_success_message "New site created successfully!"
+# STEP 15 install ERPNext latest version in bench & site
+bench get-app payments
+bench get-app erpnext --branch version-14
+bench --site {site-name} install-app erpnext
+bench start
 
-# STEP 15: Install ERPNext latest version in bench & site
-show_info_message "Installing ERPNext..."
-$ERPNEXT_APP_INSTALLATION
-show_success_message "ERPNext installed successfully!"
+#inorder to install production bench needed to be start which will be used in installing any apps so keep bench running untill apps are installing
+# Step 16 setup production
+sudo bench setup production {username}-frappe
+bench restart
 
-# Start frappe-bench
-show_prompt_message "Do you want to start the frappe-bench? (Y/n)"
-read -r start_bench
+# If bench restart is not worked run the following command again with all Questions Yes
+sudo bench setup production dcode-frappe
 
-if [[ $start_bench =~ ^[Yy]$ ]]; then
-    bench start
-fi
+#if js and css file is not loading on login window run the following command
+sudo chmod o+x /home/
 
-# STEP 16: Setup production
-show_info_message "Setting up production environment..."
-$PRODUCTION_SETUP
-show_success_message "Production environment setup completed successfully!"
+# Ask if they want to create new user, this will be used to create and save ssl details, before installing ssl ask user if they want to setup ssl
+# STEP 17 Create a new user
+sudo adduser {new-user-name}
+sudo usermod -aG sudo {new-user-name}
 
-# STEP 17: Create a new user
-show_info_message "Creating a new user..."
-$CREATE_USER
-su - dcode-frappe
-show_success_message "New user created successfully!"
+# STEP 18 SSL certificate fot https
+# ask user for domainname for ssl certificate in {domain_name}
+sudo apt install certbot python3-certbot-nginx
+certbot -d {domain_name} --register-unsafely-without-email
+su - {new-user-name}
 
-# STEP 16: SSL certificate for HTTPS
-show_info_message "Installing SSL certificate..."
-sudo apt-get install -y certbot python3-certbot-nginx
-certbot -d "$SSL_DOMAIN_NAME" --register-unsafely-without-email
-show_success_message "SSL certificate installed successfully!"
-
-# Auto renew the SSL certificate
-show_info_message "Setting up auto-renewal for the SSL certificate..."
+# for auto renew the certificate
 sudo certbot renew --dry-run
-show_success_message "SSL certificate auto-renewal setup completed successfully!"
+
